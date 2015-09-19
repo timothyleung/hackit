1b21f0b3e22088ec316d9e92e8850dcfd312b829
thang.arsenal <thang.arsenal@gmail.com>
2015-09-20 02:41:36 +1000

    Stupid click. Add missing url handler file

--- Food4Thought/core/urls.py
+++ Food4Thought/core/urls.py
@@ -2,5 +2,6 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
 		url(r'^login$', 'core.views.login',  name='login'),
-
+		url(r'^request$', 'core.order-request.request',  name='request'),
+		url(r'^send$', 'core.order-request.send',  name='send'),
 )
