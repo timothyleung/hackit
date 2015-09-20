b370c0afd74fcccdcbe70992aea58dad39bef3e1
Timothy Leung <Timothy@Timothys-MacBook-Pro.local>
2015-09-20 13:53:36 +1000

    Request sending ok

--- Food4Thought/core/urls.py
+++ Food4Thought/core/urls.py
@@ -3,8 +3,8 @@ from core.forms import *
 
 urlpatterns = patterns('',
     url(r'^$', 'core.views.home', name='home'),
-    url(r'^request$', 'core.order-request.request', name='request'),
-    url(r'^send$', 'core.order-request.send', name='send'),
+    #url(r'^request$', 'core.order-request.request', name='request'),
+    # url(r'^send$', 'core.order-request.send', name='send'),
     url(r'^login$', 'django.contrib.auth.views.login', {'template_name':'login.html',
                                                         'authentication_form':UserAuthForm,
                                                         'extra_context': {'registration_form': RegistrationForm, 'request_form':RequestForm},
@@ -12,4 +12,5 @@ urlpatterns = patterns('',
     url(r'^register$', 'core.views.register', name='register'),
     url(r'^go_to_register_page$', 'core.views.go_to_register_page', name='go_to_register_page'),
     url(r'^logout$', 'django.contrib.auth.views.logout_then_login', name='logout'),
-    )
+    url(r'^request$', 'core.views.request_order', name='request_order'),
+)
